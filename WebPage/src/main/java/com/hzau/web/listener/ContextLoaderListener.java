10d7649f0487c8a13c01e78f7f8c1f465e714f5d
Dafengsu <1187344209@qq.com>
2020-02-22 17:06:11 +0800

    2.22

--- WebPage/src/main/java/com/hzau/web/listener/ContextLoaderListener.java
+++ WebPage/src/main/java/com/hzau/web/listener/ContextLoaderListener.java
@@ -1,21 +1,41 @@
 package com.hzau.web.listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * @author su
  * @description
  * @date 2020/2/21
  */
+@WebListener
 public class ContextLoaderListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-
+        System.out.println("ServletContext对象被销毁了。。。。");
     }
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        
+        System.out.println("ServletContext对象被创建了。。。。");
+        //加载资源文件
+        //1.获取ServletContext对象
+        ServletContext servletContext = sce.getServletContext();
+        //2.加载资源文件
+        String contextConfigLocation = servletContext.getInitParameter("contextConfigLocation");
+        //3. 获取真实路径
+        String realPath = servletContext.getRealPath(contextConfigLocation);
+        //4.加载进内存
+        try {
+            FileInputStream fis = new FileInputStream(realPath);
+            System.out.println(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
